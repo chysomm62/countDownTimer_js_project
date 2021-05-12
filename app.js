43557aa9f8544bb6965819d9fb766ff0499253fc
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-05-12 21:34:13 +0100

    updated

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2021,5,20,02,50,30);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+let day = futureDate.getDay();
+day = weekdays[day]; 
+
+// add to html
+giveaway.textContent = `giveaway ends on  ${day} ${month} ${year} ${hours}: ${minutes}am`;
